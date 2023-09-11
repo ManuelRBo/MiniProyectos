86b4c0ce873434f04974e9a4d49a8b67d925d4e6
Manuel Rodrigo <dragonfire069@gmail.com>
2023-09-11 10:42:40 +0200

    Calculadora de edad

--- Calculadora Edad/src/js/utilidades/calcularEdad.js
+++ Calculadora Edad/src/js/utilidades/calcularEdad.js
@@ -7,11 +7,59 @@ function calcularEdad() {
   const mesActual = fechaActual.getMonth() + 1;
   const anioActual = fechaActual.getFullYear();
 
-  if (diaActual == dia.value && mesActual == mes.value) {
-    resultadoAños.textContent = anioActual - anio.value;
-    resultadoMes.textContent = 0;
-    resultadoDia.textContent = 0;
-  } else if (dia.value > diaActual && mes.value > mesActual){
-    resultadoAños.textContent = anioActual - anio.value - 1;
+  resultadoAños.textContent = calcularAños(anioActual, diaActual, mesActual);
+  resultadoMes.textContent = calcularMes(mesActual, diaActual);
+  resultadoDia.textContent = calcularDias(diaActual, calcularMes(mesActual, diaActual));
+
+}
+
+// Objetivo: calcular los años de una persona
+function calcularAños(anioActual, diaActual, mesActual) {
+  let años = 0;
+  if(dia.value > diaActual && mes.value >= mesActual){
+    años = anioActual - anio.value - 1;
+  }else{
+    años = anioActual - anio.value;
   }
+  return años;
+}
+
+ // Objetivo: calcular los meses de una persona
+function calcularMes(mesActual, diaActual){
+  let meses = 0;
+  let mesNacimiento = mes.value;
+  
+  if(mes.value == mesActual && dia.value > diaActual){
+    meses = 11;
+  }else{
+
+  if(mes.value == 12){
+    mesNacimiento = 0;
+  }
+
+  for(let i = mesNacimiento; i < mesActual; i++){
+    meses ++;
+    if(i == 12){
+      i = 0;
+    }
+  }
+
+  if(dia.value > diaActual){
+    meses --;
+  }
+
+}
+  return meses;
+}
+
+// Objetivo: calcular los días de una persona
+function calcularDias(diaActual, calcularMes){
+  let dias = 0;
+  if(dia.value > diaActual){
+    console.log(diaMeses[calcularMes]);
+    dias = diaMeses[calcularMes] - dia.value + diaActual;
+  }else{
+    dias = diaActual - dia.value;
+  }
+  return dias;
 }
