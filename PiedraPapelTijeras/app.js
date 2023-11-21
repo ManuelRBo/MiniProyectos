aa135cf924b88d369c4532a7d210532cd39fd7aa
ManuelRBo <dragonfire069@gmail.com>
2023-11-22 00:07:33 +0100

    queda termiar el 80% del pc

--- PiedraPapelTijeras/app.js
+++ PiedraPapelTijeras/app.js
@@ -1,5 +1,5 @@
 import { comprobarGanador } from "./src/js/jugadorVSjugador.js";
-import { jugador1, jugador2 } from "./src/js/jugadorVSjugador.js";
+import { jugador1, jugador2, jugador1vsPC } from "./src/js/jugadorVSjugador.js";
 import { objetos } from "./src/js/objetos.js";
 import { historial } from "./src/js/historial.js";
 
@@ -16,15 +16,94 @@ menuJugadorvsJugador.addEventListener("click", () => {
   //Desaparece el menu
   contenedor.removeChild(menuNivel);
   contenedorJuego.style.display = "flex";
-  jugar();
+  jugarJugadorVSjugador();
 });
 
 menuJugadorvsPc.addEventListener("click", () => {
   contenedor.removeChild(menuNivel);
   contenedorJuego.style.display = "flex";
+  jugarVsPc();
 });
 
-function jugar() {
+function jugarVsPc() {
+  contenedorJuego.innerHTML = jugador1vsPC;
+  // Empezamos el juego e inicializamos las variables
+  let eleccionJugador1 = "";
+  let eleccionJugador2 = "";
+
+  function manejarTeclado(e) {
+    if (e.key === "q" || e.key === "w" || e.key === "e" || e.key === "r") {
+        eleccionJugador1 = e.key;
+        historial.jugador1.elecciones.push(eleccionJugador1);
+        if(eleccionJugador1 === "r"){
+          eleccionJugador2 = eleccion80();
+        }else{
+          eleccionJugador2 = eleccionAleatoria();
+        }
+        historial.jugador2.elecciones.push(eleccionJugador2);
+        contenedorJuego.innerHTML = resultado(eleccionJugador1, eleccionJugador2, objetos);
+        document.body.removeEventListener("keydown", manejarTeclado);
+        setTimeout(() => {
+          contenedorJuego.innerHTML += comprobarGanador(eleccionJugador1, eleccionJugador2);
+          document.querySelector(".volverJugar").addEventListener("click", () => {
+            jugarVsPc();
+          });
+          document.querySelector(".terminar").addEventListener("click", () => {
+            let partidasTotales = historial.jugador1.partidasGanadas + historial.jugador2.partidasGanadas + historial.empate.partidasEmpatadas;
+            let partidasGanadasJugador2 = historial.jugador2.partidasGanadas;
+            let partidasGanadasJugador1 = historial.jugador1.partidasGanadas;
+            let partidasPerdidasJugador1 = partidasTotales - partidasGanadasJugador1;
+            let partidasPerdidasJugador2 = partidasTotales - partidasGanadasJugador2;
+            let porcentajeGanadasJugador1 = (partidasGanadasJugador1 * 100) / partidasTotales;
+            let porcentajeGanadasJugador2 = (partidasGanadasJugador2 * 100) / partidasTotales;
+            document.querySelector(".contenedorCentrado").style.height = "auto";
+            contenedorJuego.innerHTML = `<div class="historial">
+            <h2>Historial</h2>
+            <p class="partidasTotales">Partidas Jugadas: ${partidasTotales}</p>
+                <div class="historialJugador1">
+                    <h3>Jugador 1</h3>
+                    <p>Partidas Ganadas: ${partidasGanadasJugador1}</p>
+                    <p>Partidas Perdidas: ${partidasPerdidasJugador1}</p>
+                    <p>Porcentaje de Victoria: ${porcentajeGanadasJugador1}%</p>
+                </div>
+                <div class="historialJugador2">
+                    <h3>Jugador 2</h3>
+                    <p>Partidas Ganadas: ${partidasGanadasJugador2}</p>
+                    <p>Partidas Perdidas: ${partidasPerdidasJugador2}</p>
+                    <p>Porcentaje de Victoria: ${porcentajeGanadasJugador2}%</p>
+                </div>
+
+            <div class="eleccionesPartidas">
+                <h2>Elecciones de las Partidas</h2>
+                <div class="eleccionesJugadores">
+                    <div class="nombreJugadores">
+                        <p>Jugador 1</p>
+                        <p>Jugador 2</p>
+                    </div>
+                      <div class="conjunto">
+                      <div class="conjunto-jugador1">papel</div>
+                      <div class="conjunto-vs">VS</div>
+                      <div class="conjunto-jugador2">piedra</div>
+                      </div>
+                </div>
+            </div>
+            <div class="volverMenu"><p>Volver al Menu</p></div>
+        </div>`;
+        
+            document.querySelector(".volverMenu").addEventListener("click", () => {
+              location.reload();
+            });
+            });
+
+        }, 1000);
+  }else{
+    alert("Tecla no valida");
+  }
+}
+document.body.addEventListener("keydown", manejarTeclado);
+}
+
+function jugarJugadorVSjugador() {
   contenedorJuego.innerHTML = jugador1;
   // Empezamos el juego e inicializamos las variables
   let turno = 1;
@@ -48,7 +127,7 @@ function jugar() {
         setTimeout(() => {
           contenedorJuego.innerHTML += comprobarGanador(eleccionJugador1, eleccionJugador2);
           document.querySelector(".volverJugar").addEventListener("click", () => {
-            jugar();
+            jugarJugadorVSjugador();
           });
 
           document.querySelector(".terminar").addEventListener("click", () => {
@@ -105,8 +184,6 @@ function jugar() {
     }
   }
   document.body.addEventListener("keydown", manejarTeclado);
-
-
 }
 
 
@@ -119,4 +196,30 @@ function resultado(eleccionJugador1, eleccionJugador2, objetos) {
             <p>Jugador 2</p>
             <p>${objetos[eleccionJugador2]};</p>
         </div>`;
+}
+
+function eleccionAleatoria(){
+  let eleccion = Math.floor(Math.random() * 3);
+  if(eleccion === 0){
+    return "q";
+  }else if(eleccion === 1){
+    return "w";
+  }else{
+    return "e";
+  }
+}
+
+function eleccion80(){
+  let eleccion = Math.floor(Math.random() * 5);
+  if(eleccion === 0){
+    return "q";
+  }else if(eleccion === 1){
+    return "w";
+  }else if(eleccion === 2){
+    return "e";
+  }else if(eleccion === 3){
+    return "q";
+  }else{
+    return "w";
+  }
 }
--- PiedraPapelTijeras/src/js/jugadorVSjugador.js
+++ PiedraPapelTijeras/src/js/jugadorVSjugador.js
@@ -19,6 +19,27 @@ export const jugador1 = `
     </div>
 </div>`;
 
+export const jugador1vsPC = `
+<div class="jugador1">
+    <div class="nombreJugador">
+        <p>Jugador 1</p>
+    </div>
+    <div class="eleccionJugador">
+        <div class="piedra" id="piedra1">
+            <p>&#128074;Piedra (Q)</p>
+        </div>
+        <div class="papel" id="papel1">
+            <p>&#9995;Papel (W)</p>
+        </div>
+        <div class="tijeras" id="tijeras1">
+            <p>&#9996;Tijera (E)</p>
+        </div>
+        <div class="trampa" id="trampa">
+            <p>Trampa (R)</p>
+        </div>
+    </div>
+</div>`;
+
 export const jugador2 = `
 <div class="jugador2">
     <div class="nombreJugador">
